71c8f100be961f781373e529753582a2d1a9bdbb
Niels Bosman <nielsbosman@mail.com>
2022-05-13 13:11:51 +0200

    Changed model filename

--- api/Program.cs
+++ api/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<Airbnb2022Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("default")));
 
+builder.Services.Add
+
 builder.Services.AddScoped<IListingRepository, ListingRepository>();
 builder.Services.AddSingleton<ListingMapper>();
 
