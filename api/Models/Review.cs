8836567c8e06cbb779e08d3f36d20951a58da99b
Niels Bosman <nielsbosman@mail.com>
2022-05-13 09:45:36 +0200

    Scaffolded everything

---
+++ api/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace api.Models
+{
+    public partial class Review
+    {
+        public int ListingId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
