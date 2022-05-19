08658a25b9389edf1e05efa21a143df8486c84f7
Niels Bosman <nielsbosman@mail.com>
2022-05-19 13:49:57 +0200

    Basic sty;ing for price filter

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "rc-slider";
 import 'rc-slider/assets/index.css';
 import { Listing } from "../models/Listing";
+import styles from "../styles/Sidebar.module.css";
 
 type Props = {
   listings: Listing[],
@@ -9,22 +10,44 @@ type Props = {
 }
 
 export const Sidebar: React.FC<Props> = ({ listings, onFilter }) => {
-  const filterPrice = (minimum: number, maximum: number) => {
-    const filteredListings = listings.filter((listing) =>
-      listing.price >= minimum && listing.price <= maximum
-    )
+  const [filteredPrice, setFilteredPrice] = useState<number[]>([]);
 
+  const prices = {
+    min: listings.reduce((acc, curr) => acc.price > curr.price ? curr : acc).price,
+    max: listings.reduce((acc, curr) => acc.price < curr.price ? curr : acc).price,
+  }
+
+  const filterPrice = (minimum: number, maximum: number) => {
+    const filteredListings = listings.filter((listing) => listing.price >= minimum && listing.price <= maximum)
     onFilter(filteredListings);
   }
 
   return (
-    <Slider
-      range
-      min={0}
-      max={400}
-      defaultValue={[0, 400]}
-      step={10}
-      onChange={(prices) => filterPrice(prices[0], prices[1])}
-    />
+    <>
+      <div className={styles.heading}>
+        <h2>Filters</h2>
+        <p>Je kunt alle Airbnb locaties filteren op prijs, buurt en review</p>
+      </div>
+      <div className={styles.sidebarItem}>
+        <h4>
+          Prijs {filteredPrice.length !== 0 && `(${filteredPrice[0]} - ${filteredPrice[1]})`}
+        </h4>
+        <Slider
+          range
+          min={prices.min}
+          max={prices.max}
+          defaultValue={[prices.min, prices.max]}
+          marks={{[prices.min]: prices.min, [prices.max]: prices.max}}
+          step={10}
+          onChange={(prices) => {
+            filterPrice(prices[0], prices[1])
+
+            if (typeof(prices) !== "number") {
+              setFilteredPrice(prices);
+            }
+          }}
+        />
+      </div>
+    </>
   )
 };
