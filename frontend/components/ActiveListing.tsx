a72e20406c77733f3009d6aa7d1ad30db983b232
Niels Bosman <nielsbosman@mail.com>
2022-06-06 15:28:36 +0200

    Added detail information basic styling

--- frontend/components/ActiveListing.tsx
+++ frontend/components/ActiveListing.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ExtendedListing } from '../models/ExtendedListing'
 import styles from '../styles/ActiveListing.module.css'
-import { XIcon } from '@heroicons/react/solid'
+import { StarIcon, XIcon } from '@heroicons/react/solid'
 
 type Props = {
   listing: ExtendedListing | null;
@@ -17,7 +17,22 @@ export const ActiveListing: React.FC<Props> = ({ listing, onClose }) => {
           <div className={styles.close} onClick={() => onClose()}>
             <XIcon width={25} height={25} />
           </div>
-          <img loading="lazy" src={listing.pictureUrl} alt={listing.name} />
+          <div className={styles.row}>
+            <div>
+              <p className={styles.info}>{listing.numberOfReviews} review(s) <StarIcon width={25} height={25}/> {listing.reviewScoresRating / 10}</p>
+              <p className={styles.info}>Buurt: {listing.neighbourhood}</p>
+              <p className={styles.info}>Prijs per nacht: {listing.price}</p>
+              <p className={styles.info}>{listing.guestsIncluded} guest(s) - {listing.bedrooms} bedroom(s) - {listing.beds} bed(s)</p>
+
+              <article style={{marginTop: 30}}>
+                {listing.description}
+              </article>
+            </div>
+            <div>
+              <img loading="lazy" src={listing.pictureUrl} alt={listing.name} width={500}/>
+            </div>
+          </div>
+
         </div>
       )}
     </div>
